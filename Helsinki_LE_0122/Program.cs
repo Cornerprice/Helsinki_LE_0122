a40b609887103a8a84e610b4f0cc8633f5c26a51
Eszter Láng <lang.eszter@blathy.info>
2024-01-22 12:56:01 +0100

    Datastructure created and file read

--- Helsinki_LE_0122/Program.cs
+++ Helsinki_LE_0122/Program.cs
@@ -1,1 +1,18 @@
-﻿Console.WriteLine("Hello, World!");
+﻿string[] input = File.ReadAllLines("helsinki.txt");
+struct Helsinki
+{
+    public int helyezes;
+    public int csapatszam;
+    public string sportag;
+    public string versenyszam;
+
+    public Helsinki(string line, string line2)
+    {
+        string[] splitted = line.Split(' ');
+        helyezes = int.Parse(splitted[0]);
+        csapatszam = int.Parse(splitted[1]);
+        sportag = line2;
+        versenyszam = line2;
+    }
+}
+
